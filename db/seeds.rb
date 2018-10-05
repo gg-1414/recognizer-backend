bd0963e2a8690b311c470979942fe9395a7a548f
Flatiron School <flatironschool@Flatirons-MacBook-Air-4.local>
2018-10-05 14:32:31 -0400

    added songs

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ Emotion.create(mood: 'anger')
 Emotion.create(mood: 'neutral')
 Emotion.create(mood: 'sadness')
 Emotion.create(mood: 'surprise')
-Emotion.create(mood: 'fear') 
+Emotion.create(mood: 'fear')
 
 
 Song.create(name: 'Disarm You', artist: 'Kaskade', emotion_id:4)
@@ -74,6 +74,32 @@ Song.create(name: 'Sunset In Costa Rica', artist: 'TroyBoi', emotion_id:2)
 Song.create(name: 'Sunset In Costa Rica', artist: 'TroyBoi', emotion_id:4)
 
 Song.create(name: 'Faded', artist: 'Odesza', emotion_id:6)
+
+
+
+Song.create(name: 'IT WASNT ME', artist: 'SHAGGY',emotion_id:2)
+Song.create(name: 'September', artist: 'Earth',emotion_id:4)
+Song.create(name: 'Afterlife', artist: 'Avenged Sevenfold',emotion_id:3)
+Song.create(name: 'Afterlife', artist: 'Avenged Sevenfold',emotion_id:7)
+Song.create(name: 'Bat Country', artist: 'Avenged Sevenfold',emotion_id:7)
+Song.create(name: 'Bat Country', artist: 'Avenged Sevenfold',emotion_id:3)
+Song.create(name: 'ShortyLikeMine', artist: 'Chris Brown',emotion_id:4)
+Song.create(name: 'Lovumba', artist: 'Daddy Yankee',emotion_id:1)
+Song.create(name: 'MI REINA', artist: 'LA MAKINA',emotion_id:1)
+Song.create(name: 'Firework', artist: 'Katy Perry',emotion_id:1)
+Song.create(name: 'Paper Planes', artist: 'MIA',emotion_id:4)
+Song.create(name: 'I want it that way', artist: 'Backstreet Boys',emotion_id:1)
+Song.create(name: 'Everytime We Touch', artist: 'Cascada',emotion_id:1)
+Song.create(name: 'Whats Left Of Me', artist: 'Nick Lachey',emotion_id:5)
+Song.create(name: 'Crush', artist: 'David Archuleta',emotion_id:5)
+
+Song.create(name: 'Nobody Can Save Me', artist: 'Linkin Park',emotion_id:5)
+Song.create(name: 'Burn', artist: 'Usher',emotion_id:5)
+Song.create(name: 'Caliente', artist: 'Jay Santos',emotion_id:1)
+Song.create(name: 'Faces', artist: 'Scary Kids Scaring Kids',emotion_id:7)
+Song.create(name: 'Day N Nite', artist: 'Kid Cudi',emotion_id:4)
+Song.create(name: 'Soundtrack To My Life', artist: 'Kid Cudi',emotion_id:4)
+Song.create(name: 'You had a bad day', artist: 'Daniel',emotion_id:5)
 
 
 
